f5247ff92ffacc69d1b14970850ac37e45b1bd6d
Tiago Morgado <Tiago.alcm@icloud.com>
2023-06-07 23:37:44 +0200

    Finished Dark mode logic

---
+++ src/context.jsx
@@ -0,0 +1,26 @@
+import { useContext, createContext, useState, useEffect } from "react";
+
+const AppContext = createContext()
+
+export const AppProvider = ({children}) => {
+    const [isDarkTheme, setIsDarkTheme] = useState(false)
+
+    const toggleDarkTheme = () => {
+        const newDarkTheme = !isDarkTheme
+        setIsDarkTheme(newDarkTheme)
+        const body = document.body.classList.toggle('dark-theme', newDarkTheme)
+    }
+
+    return (
+        <AppContext.Provider 
+            value={{
+                isDarkTheme, 
+                toggleDarkTheme
+                }
+            }>
+            {children}
+        </AppContext.Provider>
+    )
+}
+
+export const useGlobalContext = () => useContext(AppContext)
