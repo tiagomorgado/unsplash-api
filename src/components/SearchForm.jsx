ef0c16b5f627d06840f346be0fdec0f28c54a06e
Tiago Morgado <Tiago.alcm@icloud.com>
2023-06-07 23:49:09 +0200

    Form Structure

--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -1,6 +1,25 @@
+import { useState } from "react"
+
 const SearchForm = () => {
+  const [searchInput, setSearchInput] = useState('')
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    const searchValue = e.target.elements.search.value
+    if(searchValue) {
+      setSearchInput(searchValue)
+    }
+  }
+
   return (
-    <div>SearchForm</div>
+    <section>
+      <h1 className='title'>Unsplash Images</h1>
+      <form className='search-form' onSubmit={() => handleSubmit}>
+        <input type='text' name='search' placeholder='cat' className='form-input search-input'/>
+        <button type='submit' className='btn'>Search</button>
+      </form>
+    </section>
+    
   )
 }
 export default SearchForm
